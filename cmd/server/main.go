0209fece1460ce4a533240fd2d25639e633b59d2
sako0 <y.m1911a1.0921@gmail.com>
2023-11-25 21:09:07 +0900

    graphQLサーバのテスト

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -24,15 +26,39 @@ func main() {
 	}
 	defer conn.Close()
 
-	// 3.HTTPサーバーの設定
+	// 3.GraphQLスキーマの定義
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world", nil
+			},
+		},
+	}
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create new GraphQL schema, error: %v", err)
+	}
+
+	// 4.GraphQLハンドラの作成
+	h := handler.New(&handler.Config{
+		Schema:     &schema,
+		Pretty:     true,
+		Playground: true,
+	})
+
+	// 5.HTTPサーバーの設定
+	http.Handle("/graphql", h)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("BFF Server is running"))
 	})
 
-	// 4.サーバーのポートを設定
+	// 6.サーバーのポートを設定
 	port := 4000
 
-	// 5.HTTPサーバーの起動
+	// 7.HTTPサーバーの起動
 	log.Printf("connect to http://localhost:%d/ for GraphQL playground", port)
 	if err := http.ListenAndServe(":"+strconv.Itoa(port), nil); err != nil {
 		log.Fatalf("failed to start HTTP server: %v", err)
--- go.mod
+++ go.mod
@@ -1,11 +1,20 @@
 module github.com/AuroralTech/todo-bff
 
-go 1.21.4
-
-require google.golang.org/grpc v1.59.0
+go 1.18
 
 require (
+	github.com/99designs/gqlgen v0.17.40
+	github.com/graphql-go/graphql v0.8.1
+	github.com/graphql-go/handler v0.2.3
+	github.com/vektah/gqlparser/v2 v2.5.10
+	google.golang.org/grpc v1.59.0
+)
+
+require (
+	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/sosodev/duration v1.1.0 // indirect
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
